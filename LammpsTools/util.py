a3542d35bc4f6658ed31cec125cf5cfe2afad298
Haoyuan Li <hyli16@stanford.edu>
2022-08-10 09:35:44 -0700

    minor

--- LammpsTools/util.py
+++ LammpsTools/util.py
@@ -91,3 +91,47 @@ def get_thermo_info_log_list(log_file_name_list):
         thermo_info_dict_tot.update({entry_name: content})
 
     return thermo_info_dict_tot
+
+
+def get_rdf_info(rdf_file):
+    """
+
+    :param rdf_file:
+    :return:
+    """
+    with open(rdf_file, 'r') as rdf_file:
+
+        # Create a holder to load the file
+        rdf_holder = []
+        step_holder = []
+
+        # Skip the header
+        for x in range(3):
+            _ = rdf_file.readline()
+
+        # Load a new line
+        line = rdf_file.readline()
+        while line:
+
+            # Parse the new line
+            words = line.split()
+
+            # If there are only two words, then this is a description line
+            if len(words) == 2:
+
+                # Save the step number
+                step_holder.append(int(words[0]))
+
+                # Create a new entry in the rdf holder
+                # Because a new description line
+                # means the following data are from a new MD snapshot
+                rdf_holder.append([])
+
+            # otherwise, this is a data line
+            else:
+                rdf_holder[-1].append([float(words[y]) for y in range(1, 4)])
+
+            # Load a new line
+            line = rdf_file.readline()
+
+    return np.array(step_holder, dtype=np.int64), np.array(rdf_holder, dtype=np.float64)
